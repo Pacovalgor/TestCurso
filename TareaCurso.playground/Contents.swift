49d0fadf58e66b723757deb81d5a682b57aa5440
Francisco Valiente <Paco@MacBook-Pro-de-Paco.local>
2015-12-06 18:05:09 +0100

    modificacion

--- TareaCurso.playground/Contents.swift
+++ TareaCurso.playground/Contents.swift
@@ -5,15 +5,12 @@ import UIKit
 for i in 1...100{
     //print(i)
     if (i % 5 == 0){
-    print("\(i) Bingo!!!")
-    }
-    else if (i % 5 == 0){
         print("\(i) Bingo!!!")
     }
-    else if (i % 2 == 0){
+    if (i % 2 == 0){
         print("\(i)  Par!!!")
     }
-    else if (i % 2 == 1){
+    if (i % 2 == 1){
         print("\(i)  Impar!!!")
     }
     if (i >= 30 && i <= 40){ //si ambos estan incluidos
